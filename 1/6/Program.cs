54377b0c7a0e3a7524be6d017ebc21e1df996bc4
VloBoo <vlobo2004@gmail.com>
2022-09-01 19:36:38 +0300

    01.09.2022 - First lab - Add new code

---
+++ 1/6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1_6
+{
+    class lab1_6
+    {
+        //Variant 26
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a 'a': ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double z1 = (1 - 2 * Math.Pow(Math.Sin(a), 2)) / (1 + Math.Sin(2 * a));
+            double z2 = (1 - Math.Tan(a)) / (1 + Math.Tan(a));
+
+            Console.WriteLine($"z1 = {z1}");
+            Console.WriteLine($"z2 = {z2}");
+        }
+    }
+}
---
+++ 1/7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace lab1_7
+{
+    class lab1_7
+    {
+        //Variant 26
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a nubmer 'x': ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a nubmer 'y': ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a nubmer 'z': ");
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            double check1 = x + y;
+            if(check1 <= 0 ){
+                Console.WriteLine("Error");
+                return;
+            }
+
+            double check2 = Math.Exp(Math.Abs(x-y))+x/2.0;
+            if(check2 == 0 ){
+                Console.WriteLine("Error");
+                return;
+            }
+
+            double b = Math.Pow(y,Math.Pow(Math.Abs(x),1/3.0)) + Math.Pow(Math.Cos(y),3) * (Math.Abs(x-y) * (1 + Math.Pow(z,2)/Math.Sqrt(check1)))/(check2);
+            
+
+            Console.WriteLine($"b = {b}");
+            
+        }
+    }
+}
